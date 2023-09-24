34848f8b2bdebacdb5abb65fec3a99f9ddc2de59
Lolinonusos <linus.nordbakken@gmail.com>
2023-09-24 16:41:33 +0200

    Ball kollisjon

--- Assets/Scripts/RollingBall.cs
+++ Assets/Scripts/RollingBall.cs
@@ -6,21 +6,29 @@ public class RollingBall : MonoBehaviour {
 
     [SerializeField] TriangleSurface triangleSurface;
 
-    float RADIUS = 3f;
+    [SerializeField][Range(0.0f, 10.0f)] private float bouncyness = 1.0f;
     
-    Vector3 GRAVITY = new Vector3(0.0f, -9.8f, 0.0f);
+    float radius = 3f;
 
-    Vector3 VELOCITY;
-    Vector3 ACCELERATION;
-    float MASS = 3.0f;
+    Vector3 gravity = Physics.gravity;
+
+    Vector3 currentVelocity;
+    Vector3 newVelocity;
+    Vector3 acceleration;
+    float mass = 3.0f;
     float KG; 
-    float NEWTON;
+    float newton;
     double TIME;
+    
+    Vector3 previousPosition;
+    Vector3 newPosition;
     
     // Start is called before the first frame update
     void Awake() {
+        previousPosition = transform.position;
+        newPosition = transform.position;
         TIME = 0.0f;
-        transform.localScale = new Vector3(RADIUS * 2.00f, RADIUS * 2.00f, RADIUS * 2.00f);
+        transform.localScale = new Vector3(radius * 2.00f, radius * 2.00f, radius * 2.00f);
         
         //VELOCITY = Vector3.ProjectOnPlane(VELOCITY, planeNormal);
     }
@@ -30,28 +38,55 @@ public class RollingBall : MonoBehaviour {
         TIME += Time.deltaTime;
 
         
+        
         //Vector3 newPosition = new Vector3();
         //Vector3 previousPosition = new Vector3();
-        Vector3 previousPosition = transform.position;
+        //previousPosition = transform.position;
+
+        //newton = gravity * mass
         
-        transform.position = previousPosition + ApplyForces() * Time.fixedDeltaTime;
-        //transform.position += ApplyForces();
+        if (CheckCollision()) {
+            Vector3 normalForce = -Vector3.Dot(triangleSurface.normalVector, gravity * mass) * triangleSurface.normalVector;
+            acceleration = (normalForce + gravity) / mass;
+            Vector3 Vnormal = Vector3.Dot(currentVelocity, triangleSurface.normalVector) * triangleSurface.normalVector; 
+            newVelocity = currentVelocity - 2*Vnormal;
+        }
+        else {
+            acceleration = gravity;
+            newVelocity = currentVelocity + acceleration * Time.fixedDeltaTime;
+            currentVelocity = newVelocity;
+        }
+        
+        newPosition = previousPosition + newVelocity * Time.fixedDeltaTime;
+        previousPosition = newPosition;
+        transform.position = newPosition;
     }
 
     Vector3 ApplyForces() {
 
-        ACCELERATION = GRAVITY;
         
-        Vector3 newVelocity = new Vector3();
-        newVelocity = VELOCITY + ACCELERATION * Time.deltaTime;
-        VELOCITY = newVelocity;
 
-        float distance = Vector3.Distance(transform.position, triangleSurface.baryc(new Vector2(transform.position.x, transform.position.z)));
-        if (distance < RADIUS) {
-            print("cumming!!!!!");
-        }
         
-        return VELOCITY;
+        
+        return newVelocity;
+    }
+
+    private bool CheckCollision()
+    {
+        // Using (k = C + ((S - C) . n) * n when |(S - C) . n| <= r) to calculate the collision point
+        Vector3 pos = transform.position; // C
+        Vector3 baryc = triangleSurface.baryc(new Vector2(pos.x, pos.z)); // S
+        Vector3 normalVec = triangleSurface.normalVector; // n
+
+        float dotProduct = Vector3.Dot(baryc - pos, normalVec);
+        
+        if (Mathf.Abs(dotProduct) <= radius)
+        {
+            Vector3 collisionPos = pos + dotProduct * normalVec;
+            //Debug.Log("Absolute value: " + Mathf.Abs(dotProduct));// + "\nCollision point: " + collisionPos);
+            return true;
+        }
+        return false;
     }
     
     void SetVelocity() {
--- Assets/Scripts/TriangleSurface.cs
+++ Assets/Scripts/TriangleSurface.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class TriangleSurface : MonoBehaviour {
 
     Vector3[] newVertices;
-    Vector2[] newUV;
     int[] newTriangles = {
         0, 3, 1,
         1, 3, 4,
@@ -20,7 +19,11 @@ public class TriangleSurface : MonoBehaviour {
     
     [SerializeField]string vertexData;
     [SerializeField]string indicesAndNeighbourData;
-    
+
+    public Vector3 normalVector;
+
+    private int previousTriangle = -1;
+    private int currentTriangle = 0;
     // Start is called before the first frame update
     void Start() {
         Mesh mesh = new Mesh();
@@ -55,8 +58,7 @@ public class TriangleSurface : MonoBehaviour {
         mesh.vertices = newVertices;
         mesh.triangles = newTriangles;
 
-        longString = string.Empty;
-        longString = indicesAndNeighbourData;
+        mesh.RecalculateNormals();
 
 
         // Place indices and neighbours
@@ -71,6 +73,7 @@ public class TriangleSurface : MonoBehaviour {
         Vector3 v2 = new Vector3();
         Vector3 v3 = new Vector3();
 
+        Vector3 baryc = new Vector3(-1, -1 , -1);
         
         for (int i = 0; i < newTriangles.Length / 3; i++) {
             int i1 = newTriangles[i * 3];
@@ -81,9 +84,21 @@ public class TriangleSurface : MonoBehaviour {
             v2 = newVertices[i2];
             v3 = newVertices[i3];
 
+            baryc = getBarycentricCoordinate(new Vector2(v1.x, v1.z), new Vector2(v2.x, v2.z), new Vector2(v3.x, v3.z), objectPosition);
+            if (baryc is { x: >= 0, y: >= 0, z: >= 0 }) {
+                currentTriangle = i;
+                break;
+            }
         }
 
-        return getBarycentricCoordinate(new Vector2(v1.x, v1.z), new Vector2(v2.x, v2.z), new Vector2(v3.x, v3.z), objectPosition);
+        // Check if we are in a different triangle, update normal vector if true
+        if (previousTriangle != currentTriangle) {
+            previousTriangle = currentTriangle;
+            CalculateNormalVector(v1, v2, v3);
+            print("Entered triangle number: " + currentTriangle);
+        }
+
+        return baryc.x * v1 + baryc.y * v2 + baryc.z * v3;
     }
 
     Vector3 getBarycentricCoordinate(Vector2 a, Vector2 b, Vector2 c, Vector2 x) {
@@ -98,11 +113,22 @@ public class TriangleSurface : MonoBehaviour {
         float d20 = Vector2.Dot(v2, v0);
         float d21 = Vector2.Dot(v2, v1);
         float denom = d00 * d11 - d01 * d01;
-            
+
         float v = (d11 * d20 - d01 * d21) / denom;
         float w = (d00 * d21 - d01 * d20) / denom;
         float u = 1.0f - v - w;
 
         return new Vector3(u, v, w);
+    }
+    
+    private void CalculateNormalVector(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        // Calculates two vector along the triangle's edge
+        Vector3 v1 = p2 - p1;
+        Vector3 v2 = p3 - p1;
+    
+        // Calculates the cross product of the two vectors to get the normal vector
+        normalVector = Vector3.Cross(v1, v2).normalized;
+        //print("Triangle normal" + normalVector + " Magnitude: " + normalVector.magnitude);
     }
 }
