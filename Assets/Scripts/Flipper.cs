1a736fb76b0348a7fe9871b3f9d4fa3a78f61de2
Lolinonusos <89453125+Lolinonusos@users.noreply.github.com>
2023-09-30 19:59:42 +0200

    Left and right flipper

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -3,18 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flipper : MonoBehaviour {
-    [SerializeField] AnimationCurve animationCurve;
+    [SerializeField] AnimationCurve animationCurveUp;
+    float up = 1f;
+    [SerializeField] AnimationCurve animationCurveDown;
+    float down = 1f;
+    AnimationCurve animationCurve;
     float _timer = 1f;
+
+    Rigidbody rb;
+    Quaternion startRot;
+    [SerializeField] bool isLeftFlipper;
+    
+    void Start() {
+	    rb = GetComponent<Rigidbody>();
+	    startRot = transform.rotation;
+	    animationCurve = animationCurveDown;
+    }
     
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.A)) {
-			_timer = 0f;
+        if (Input.GetKeyDown(KeyCode.A) && isLeftFlipper) {
+	        Flip(animationCurveUp);
+        }
+        else if(Input.GetKeyDown(KeyCode.D) && !isLeftFlipper){
+	        Flip(animationCurveUp);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) && isLeftFlipper) {
+	        Flip(animationCurveDown);
+        }
+        else if (Input.GetKeyUp(KeyCode.D) && !isLeftFlipper) {
+	        Flip(animationCurveDown);
         }
 
         _timer += Time.deltaTime;
-	    float angleRot = animationCurve.Evaluate(_timer) * 45f;
-	    Vector3 localRotation = new Vector3(0, 0, angleRot);
-        transform.localEulerAngles = localRotation;
+	    // float angleRot = animationCurve.Evaluate(_timer) * 45f;
+	    // Vector3 localRotation = new Vector3(angleRot, 0, 0);
+		// transform.localEulerAngles = localRotation;
+        rb.MoveRotation(startRot * Quaternion.Euler(animationCurve.Evaluate(_timer) * 45f,0,0));
+    }
+
+    void Flip(AnimationCurve curve) {
+	    _timer = 0f;
+	    animationCurve = curve;
     }
 }
